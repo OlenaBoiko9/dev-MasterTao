6dd361cbf26d66225aebbc155f63363ba8c3e422
OlenaBoiko9 <boiko.olena.2324ls@gmail.com>
2024-01-21 15:47:32 +0200

    feat: first-screen

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,8 +1,22 @@
-const burger = document.querySelector('.btn-burger');
-const nav = document.querySelector('.header__nav');
+  $(document).ready(function(){
+      $('#banner').slick({
+          // setting-name: setting-value
+          arrows: false,
+          dots: false,
+          fade: true,
 
-function burgerMenu () {
-burger.classList.toggle('open');
-nav.classList.toggle('show');
-}
-burger.addEventListener('click', burgerMenu);
+      });
+  
+      const burger = document.querySelector('.btn-burger');
+      const nav = document.querySelector('.header__nav');
+  
+      const toggleMenu =()=> {    
+          burger.classList.toggle('open');
+          nav.classList.toggle('show');
+      }
+  
+      burger.addEventListener('click', toggleMenu);
+  
+  });
+  
+  
