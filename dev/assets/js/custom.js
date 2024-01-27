448be59d6286cf675584643a33dbbc8a940bca8e
OlenaBoiko9 <boiko.olena.2324ls@gmail.com>
2024-01-27 22:41:53 +0200

    feat: calculation form

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -24,7 +24,19 @@
       }
   
       burger.addEventListener('click', toggleMenu);
-  
+
+      const fileInput = document.querySelector('#fileInput');
+      const fileName = document.querySelector('#file-name');
+
+      fileInput.addEventListener('change', function () {
+        // fileName.innerHTML =  fileInput.value;   long name
+        fileName.innerHTML = this.files[0].name;   // short name
+      })
+     
+      const typeTheckbox = document.querySelector('[type=checkbox]');
+
+      typeTheckbox.closest('.input-column').classList.add('column-checkbox')
+
   });
   
   
